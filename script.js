69e63d32c1033b2b3be1fbcfad5bfd8eaaea3210
Luca De Santis <lucaroma2705@gmail.com>
2022-01-09 02:23:08 +0100

    inizio javascript

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
+const pen = document.querySelector("#pen-color");
+const background = document.querySelector("#background-color");
 
 let n = 16 ** 2;
 
+let color = '#000';
+
+pen.addEventListener('input', (e) => {
+        color = e.target.value;
+    });
+
 for (let i = 0; i < n; i++) {
-    const smallSquare = document.createElement('div');
-    smallSquare.classList.add('small-square');
-    container.appendChild(smallSquare);
+  const smallSquare = document.createElement("div");
+  smallSquare.classList.add("small-square");
+  container.appendChild(smallSquare);
+  ['mousedown', 'mouseenter'].forEach( event => {
+      smallSquare.addEventListener(event, () => {
+          smallSquare.style.backgroundColor = color;
+      })
+  })
 }
+
+
