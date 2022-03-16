ba185bfa79fb664cb5f84f31a2a3f2307001fcfb
Luca De Santis <lucaroma2705@gmail.com>
2022-03-16 21:35:43 +0100

    modificato js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//HTML elements
 const container = document.querySelector(".container");
 const penColor = document.querySelector("#pen-color");
 const background = document.querySelector("#background-color");
@@ -6,7 +7,7 @@ const rainbow = document.querySelector("#rainbow-card");
 const random = document.querySelector("#random-card");
 const btnClear = document.querySelector(".clear");
 const gridTemplate = document.querySelector("#grid-template");
-const h3 = document.querySelector("h3");
+const gridSizeDisplay = document.querySelector("h3");
 const penAnimation = document.querySelector("#behind-pen");
 const rainbowAnimation = document.querySelector("#behind-rainbow");
 const randomAnimation = document.querySelector("#behind-random");
@@ -14,43 +15,115 @@ const info = document.querySelector("#info-btn");
 const reference = document.querySelector(".reference");
 const gridBtn = document.querySelector("#grid-card");
 const gridAnimation = document.querySelector("#behind-grid");
-const rubberBtn = document.querySelector("#rubber-card");
+const rubber = document.querySelector("#rubber-card");
 const rubberAnimation = document.querySelector("#behind-rubber");
-const lightenBtn = document.querySelector("#lighten-card");
+const lighten = document.querySelector("#lighten-card");
 const lightenAnimation = document.querySelector("#behind-lighten");
 
-let n = +JSON.parse(localStorage.getItem("num")) || 16;
-h3.innerHTML = `${n}x${n}`;
-document.querySelector('input[type="range"]').value = n;
-
+//global var
+let gridSize = +JSON.parse(localStorage.getItem("num")) || 16;
 let smallSquares;
-
 let color = JSON.parse(localStorage.getItem("color")) || "#000";
-penColor.value = color;
-
 let bkgColor = JSON.parse(localStorage.getItem("bkgColor")) || "#fff";
+let actionItem = JSON.parse(localStorage.getItem("actionItem")) || "pen";
+let mousedown = false;
+
+//active local storage
+gridSizeDisplay.innerHTML = `${gridSize}x${gridSize}`;
+document.querySelector('input[type="range"]').value = gridSize;
+penColor.value = color;
 background.value = bkgColor;
 container.style.backgroundColor = bkgColor;
 
-let checkPrint = JSON.parse(localStorage.getItem("checkPrint")) || "pen";
+createGrid();
+randomBkgColor();
 
-let mousedown = false;
 
-function switchColor() {
-  penAnimation.classList.remove("color-active");
-  randomAnimation.classList.remove("color-active");
-  rainbowAnimation.classList.remove("color-active");
-  switch (checkPrint) {
+
+penColor.addEventListener("input", changePenColor);
+background.addEventListener("input", changeBkgColor);
+
+pen.addEventListener("click", switchItem.bind(this, ["pen"]));
+rainbow.addEventListener("click", switchItem.bind(this, ["rainbow"]));
+random.addEventListener("click", switchItem.bind(this, ["random"]));
+lighten.addEventListener("click", switchItem.bind(this, ["lighten"]));
+rubber.addEventListener("click", switchItem.bind(this, ["rubber"]));
+
+
+
+gridTemplate.addEventListener("input", changeGridTemplate);
+
+btnClear.addEventListener("click", clear);
+
+info.addEventListener("mouseover", removeClass);
+
+info.addEventListener("mouseleave", addClass);
+
+gridBtn.addEventListener("click", toggleGrid);
+
+
+
+function createGrid() {
+  const squares = JSON.parse(localStorage.getItem("squares")) || [];
+  container.style.cssText += `grid-template-columns: repeat(${gridSize}, 1fr); grid-template-rows: repeat(${gridSize}, 1fr)`;
+  for (let i = 0; i < gridSize ** 2; i++) {
+    const smallSquare = document.createElement("div");
+    smallSquare.classList.add("small-square");
+    if (squares[i] && squares[i] !== "#0") {
+      smallSquare.style.backgroundColor = squares[i];
+    }
+    if (
+      gridAnimation.classList.length > 1 ||
+      JSON.parse(localStorage.getItem("grid")) === true
+    ) {
+      gridAnimation.classList.add("option-active");
+      smallSquare.classList.add("border-top-left");
+    }
+    container.appendChild(smallSquare);
+    smallSquare.addEventListener("mousedown", (e) => {
+      mousedown = true;
+      action(e.target);
+    });
+    smallSquare.addEventListener("mouseup", () => {
+      mousedown = false;
+    });
+    smallSquare.addEventListener("mouseenter", (e) => {
+      if (mousedown) {
+        action(e.target);
+      }
+    });
+  }
+  smallSquares = document.querySelectorAll(".small-square");
+}
+
+function action(smallSquare) {
+  const squareStyle = smallSquare.style;
+  switch (actionItem) {
     case "pen":
-      penAnimation.classList.add("color-active");
+      squareStyle.backgroundColor = color;
       break;
     case "rainbow":
-      rainbowAnimation.classList.add("color-active");
+      squareStyle.backgroundColor = rainbowColor();
       break;
     case "random":
-      randomAnimation.classList.add("color-active");
+      squareStyle.backgroundColor = randomRGB();
+      break;
+    case "lighten":
+      squareStyle.backgroundColor = LightenDarkenColor(RGBToHex(squareStyle.backgroundColor), 30);
+      break;
+    case "rubber":
+      squareStyle.backgroundColor = "transparent";
       break;
   }
+  localStorage.setItem("squares", JSON.stringify(squaresToArrOfHex()));
+}
+
+function randomColor() {
+  let colors = [];
+  for (let i = 0; i < 3; i++) {
+    colors[i] = Math.floor(Math.random() * 255);
+  }
+  return colors;
 }
 
 function randomRGB() {
@@ -71,144 +144,9 @@ function rainbowColor() {
   return rainbowColors[Math.floor(Math.random() * 6)];
 }
 
-function draw(smallSquare) {
-  switch (checkPrint) {
-    case "pen":
-      smallSquare.style.backgroundColor = color;
-      break;
-    case "rainbow":
-      smallSquare.style.backgroundColor = rainbowColor();
-      break;
-    case "random":
-      smallSquare.style.backgroundColor = randomRGB();
-      break;
-  }
-  localStorage.setItem("squares", JSON.stringify(squaresToArrOfHex()));
-}
-
-const createGrid = () => {
-  const squares = JSON.parse(localStorage.getItem("squares")) || [];
-  container.style.cssText += `grid-template-columns: repeat(${n}, 1fr); grid-template-rows: repeat(${n}, 1fr)`;
-  for (let i = 0; i < n ** 2; i++) {
-    const smallSquare = document.createElement("div");
-    smallSquare.classList.add("small-square");
-    if(squares[i] && squares[i] !== "#0"){
-      smallSquare.style.backgroundColor = squares[i];
-    }
-    if (
-      gridAnimation.classList.length > 1 ||
-      JSON.parse(localStorage.getItem("grid")) === true
-    ) {
-      gridAnimation.classList.add("option-active");
-      smallSquare.classList.add("border-top-left");
-    }
-    container.appendChild(smallSquare);
-    smallSquare.addEventListenerWithName("printMouseDown", "mousedown", () => {
-      mousedown = true;
-      draw(smallSquare);
-    });
-    smallSquare.addEventListenerWithName("dontPrintMouseUp", "mouseup", () => {
-      mousedown = false;
-    });
-    smallSquare.addEventListenerWithName("printMouseEnter", "mouseenter", () => {
-      if (mousedown) {
-        draw(smallSquare);
-      }
-    });
-  }
-  smallSquares = document.querySelectorAll(".small-square");
-};
-
-createGrid();
-
-if(JSON.parse(localStorage.getItem("rubber"))){
-  rubber();
-} else {
-  switchColor();
-}
-
-penColor.addEventListener("input", (e) => {
-  e.stopImmediatePropagation();
-  color = e.target.value;
-  localStorage.setItem("color", JSON.stringify(color));
-});
-
-pen.addEventListener("click", () => {
-  checkPrint = "pen";
-  localStorage.setItem("checkPrint", JSON.stringify(checkPrint));
-  localStorage.setItem("rubber", JSON.stringify(false));
-  switchColor();
-  rubberAnimation.classList.remove("option-active");
-  smallSquares.forEach(smallSquare => {
-    smallSquare.removeEventListenerWithName("rubberClick");
-    smallSquare.addEventListenerWithName("printMouseDown");
-    smallSquare.addEventListenerWithName("printMouseEnter");
-  });
-});
-
-rainbow.addEventListener("click", () => {
-  checkPrint = "rainbow";
-  localStorage.setItem("checkPrint", JSON.stringify(checkPrint));
-  localStorage.setItem("rubber", JSON.stringify(false));
-  switchColor();
-  rubberAnimation.classList.remove("option-active");
-  smallSquares.forEach(smallSquare => {
-    smallSquare.removeEventListenerWithName("rubberClick");
-    smallSquare.addEventListenerWithName("printMouseDown");
-    smallSquare.addEventListenerWithName("printMouseEnter");
-  });
-});
-
-background.addEventListener("input", (e) => {
-  bkgColor = e.target.value;
-  container.style.backgroundColor = bkgColor;
-  localStorage.setItem("bkgColor", JSON.stringify(bkgColor));
-});
-
-gridTemplate.addEventListener("input", (e) => {
-  n = e.target.value;
-  localStorage.setItem("num", JSON.stringify(n));
-  localStorage.removeItem("squares");
-  h3.innerText = `${n}x${n}`;
-  container.replaceChildren([]);
-  createGrid();
-});
-
-const randomColor = () => {
-  let colors = [];
-  for (let i = 0; i < 3; i++) {
-    colors[i] = Math.floor(Math.random() * 255);
-  }
-  return colors;
-};
-
-function randomBckColor() {
+function randomBkgColor() {
   random.children[1].style.cssText = `background: linear-gradient(-45deg, ${randomRGB()}, ${randomRGB()}, ${randomRGB()}, ${randomRGB()}, ${randomRGB()}, ${randomRGB()}, ${randomRGB()});`;
 }
-
-randomBckColor();
-
-random.addEventListener("click", () => {
-  checkPrint = "random";
-  localStorage.setItem("checkPrint", JSON.stringify(checkPrint));
-  localStorage.setItem("rubber", JSON.stringify(false));
-  switchColor();
-  randomBckColor();
-  rubberAnimation.classList.remove("option-active");
-  smallSquares.forEach(smallSquare => {
-    smallSquare.removeEventListenerWithName("rubberClick");
-    smallSquare.addEventListenerWithName("printMouseDown");
-    smallSquare.addEventListenerWithName("printMouseEnter");
-  });
-});
-
-btnClear.addEventListener("click", () => {
-  smallSquares.forEach((smallSquare) => {
-    smallSquare.style.backgroundColor = "transparent";
-  });
-  container.style.backgroundColor = bkgColor;
-  localStorage.removeItem("squares");
-});
 
 function addClass() {
   setTimeout(() => {
@@ -220,23 +158,9 @@ function removeClass() {
   reference.classList.remove("visibility");
 }
 
-info.addEventListener("mouseover", removeClass);
-
-info.addEventListener("mouseleave", addClass);
-
-gridBtn.addEventListener("click", () => {
-  gridAnimation.classList.toggle("option-active");
-  smallSquares.forEach((smallSquare) => {
-    smallSquare.classList.toggle("border-top-left");
-  });
-  localStorage.setItem("grid", gridAnimation.classList.contains("option-active"));
-});
-
-rubberBtn.addEventListener("click", rubber);
-
-function rubber(){
+function rubberFun() {
   rubberAnimation.classList.toggle("option-active");
-  if(rubberAnimation.classList.contains("option-active")){
+  if (rubberAnimation.classList.contains("option-active")) {
     localStorage.setItem("rubber", JSON.stringify(true));
     penAnimation.classList.remove("color-active");
     randomAnimation.classList.remove("color-active");
@@ -252,7 +176,7 @@ function rubber(){
   } else {
     localStorage.setItem("rubber", JSON.stringify(false));
     switchColor();
-    smallSquares.forEach(smallSquare => {
+    smallSquares.forEach((smallSquare) => {
       smallSquare.removeEventListenerWithName("rubberClick");
       smallSquare.addEventListenerWithName("printMouseDown");
       smallSquare.addEventListenerWithName("printMouseEnter");
@@ -262,36 +186,20 @@ function rubber(){
 
 function RGBToHex(RGB) {
   if (!RGB.match("rgb")) return "#0";
-  if(RGB.match("#")) return RGB;
+  if (RGB.match("#")) return RGB;
   return RGB.slice(4, RGB.length - 1)
     .split(",")
-    .reduce((hex, color) => (hex += Number(color).toString(16).padStart(2, "0")), "#");
+    .reduce(
+      (hex, color) => (hex += Number(color).toString(16).padStart(2, "0")),
+      "#"
+    );
 }
 
 function squaresToArrOfHex() {
-  return [...document.querySelectorAll(".small-square")].map((el) => 
+  return [...document.querySelectorAll(".small-square")].map((el) =>
     RGBToHex(el.style.backgroundColor)
   );
 }
-
-lightenBtn.addEventListener("click", () => {
-  lightenAnimation.classList.toggle("option-active");
-  if(lightenAnimation.classList.contains("option-active")){
-    smallSquares.forEach(smallSquare => {
-      smallSquare.removeEventListenerWithName("rubberClick");
-      smallSquare.removeEventListenerWithName("printMouseDown");
-      smallSquare.removeEventListenerWithName("printMouseEnter");
-      smallSquare.addEventListenerWithName("lightenClick", "click", (e) => {
-        const elStyle = e.target.style;
-        let bgColor = elStyle.backgroundColor;
-        elStyle.backgroundColor = LightenDarkenColor(RGBToHex(bgColor), 30);
-        localStorage.setItem("squares", JSON.stringify(squaresToArrOfHex()));
-      });
-    });
-  } else {
-
-  }
-});
 
 function LightenDarkenColor(col, amt) {
   let usePound = false;
@@ -318,4 +226,75 @@ function LightenDarkenColor(col, amt) {
   else if (g < 0) g = 0;
 
   return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
+}
+
+function switchItem([item]) {
+  actionItem = item;
+
+  penAnimation.classList.remove("color-active");
+  rainbowAnimation.classList.remove("color-active");
+  randomAnimation.classList.remove("color-active");
+  lightenAnimation.classList.remove("option-active");
+  rubberAnimation.classList.remove("option-active");
+
+  switch (item) {
+    case "pen":
+      penAnimation.classList.add("color-active");
+      break;
+    case "rainbow":
+      rainbowAnimation.classList.add("color-active");
+      break;
+    case "random":
+      randomAnimation.classList.add("color-active");
+      break;
+    case "lighten":
+      lightenAnimation.classList.add("option-active");
+      break;
+    case "rubber":
+      rubberAnimation.classList.add("option-active");
+      break;
+    default:
+      console.log("error");
+      break;
+  }
+}
+
+function clear() {
+  smallSquares.forEach((smallSquare) => {
+    smallSquare.style.backgroundColor = "transparent";
+  });
+  container.style.backgroundColor = bkgColor;
+  localStorage.removeItem("squares");
+}
+
+function changePenColor(e) {
+  e.stopImmediatePropagation();
+  color = e.target.value;
+  localStorage.setItem("color", JSON.stringify(color));
+}
+
+function changeBkgColor(e) {
+  bkgColor = e.target.value;
+  container.style.backgroundColor = bkgColor;
+  localStorage.setItem("bkgColor", JSON.stringify(bkgColor));
+}
+
+function changeGridTemplate(e) {
+  gridSize = e.target.value;
+  localStorage.setItem("num", JSON.stringify(gridSize));
+  localStorage.removeItem("squares");
+  gridSizeDisplay.innerText = `${gridSize}x${gridSize}`;
+  container.replaceChildren([]);
+  createGrid();
+}
+
+function toggleGrid() {
+  gridAnimation.classList.toggle("option-active");
+  smallSquares.forEach((smallSquare) => {
+    smallSquare.classList.toggle("border-top-left");
+  });
+  localStorage.setItem(
+    "grid",
+    gridAnimation.classList.contains("option-active")
+  );
 }
